d164123282b2cd35fa7e2e0163f98c1af4c80ff8
Sharan Phadke <phadkesharan@gmail.com>
2022-05-06 22:01:01 +0530

    NEW: post edit

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -1,76 +1,107 @@
-import { Link, useLocation } from "react-router-dom";
-import { useState, useEffect, useContext } from "react";
-import "./singlePost.css";
 import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { useLocation } from "react-router";
+import { Link } from "react-router-dom";
 import { Context } from "../../context/Context";
+import "./singlePost.css";
 
 export default function SinglePost() {
-
-  const IMG_FOLDER = "http://localhost:8000/images/";
   const location = useLocation();
-  const postId = location.pathname.split('/')[2];
-  console.log(postId);
-
+  const path = location.pathname.split("/")[2];
+  const [post, setPost] = useState({});
+  const PF = "http://localhost:5000/images/";
   const { user } = useContext(Context);
-  const [post, setPost] = useState({}); 
-
-  const handleDelete = async () => {
-    console.log("context user", user.username);
-    try{
-      const res = await axios.delete(`/posts/${postId}`, {
-        data: {
-          username: user.username
-        }
-      });
-      window.location.replace("/");
-      console.log(res);
-    }
-    catch(err) {
-      console.log(err);
-    }
-  }
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [updateMode, setUpdateMode] = useState(false);
 
   useEffect(() => {
-
     const getPost = async () => {
-      const postResponse = await axios.get(`/posts/${postId}`);
-      console.log(postResponse.data);
-      setPost(postResponse.data);
-    }
-
+      const res = await axios.get("/posts/" + path);
+      setPost(res.data);
+      setTitle(res.data.title);
+      setDesc(res.data.desc);
+    };
     getPost();
+  }, [path]);
 
-  }, [])
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/posts/${post._id}`, {
+        data: { username: user.username },
+      });
+      window.location.replace("/");
+    } catch (err) {}
+  };
+
+  const handleUpdate = async () => {
+    try {
+      await axios.put(`/posts/${post._id}`, {
+        username: user.username,
+        title,
+        desc,
+      });
+      setUpdateMode(false)
+    } catch (err) {}
+  };
 
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
-        <img
-          className="singlePostImg"
-          src={IMG_FOLDER + post.img}
-          alt=""
-        />
-        <h1 className="singlePostTitle">
-          {post.title}
-          {post.username === user.username && <div className="singlePostEdit">
-            <i className="singlePostIcon far fa-edit"></i>
-            <i className="singlePostIcon far fa-trash-alt" onClick={handleDelete}></i>
-          </div>}
-        </h1>
+        {post.photo && (
+          <img src={PF + post.photo} alt="" className="singlePostImg" />
+        )}
+        {updateMode ? (
+          <input
+            type="text"
+            value={title}
+            className="singlePostTitleInput"
+            autoFocus
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        ) : (
+          <h1 className="singlePostTitle">
+            {title}
+            {post.username === user?.username && (
+              <div className="singlePostEdit">
+                <i
+                  className="singlePostIcon far fa-edit"
+                  onClick={() => setUpdateMode(true)}
+                ></i>
+                <i
+                  className="singlePostIcon far fa-trash-alt"
+                  onClick={handleDelete}
+                ></i>
+              </div>
+            )}
+          </h1>
+        )}
         <div className="singlePostInfo">
-          <span>
+          <span className="singlePostAuthor">
             Author:
-            <b className="singlePostAuthor">
-              <Link className="link" to={`/posts?username=${post.username}`}>
-                {post.username}
-              </Link>
-            </b>
+            <Link to={`/?user=${post.username}`} className="link">
+              <b> {post.username}</b>
+            </Link>
           </span>
-          <span>{new Date(post.createdAt).toDateString()}</span>
+          <span className="singlePostDate">
+            {new Date(post.createdAt).toDateString()}
+          </span>
         </div>
-        <p className="singlePostDesc">
-          {post.desc}
-        </p>
+        {updateMode ? (
+          <textarea
+            className="singlePostDescInput"
+            value={desc}
+            rows={10}
+            onChange={(e) => setDesc(e.target.value)}
+          />
+        ) : (
+          <p className="singlePostDesc">{desc}</p>
+        )}
+        {updateMode && (
+          <button className="singlePostButton" onClick={handleUpdate}>
+            Update
+          </button>
+        )}
       </div>
     </div>
   );
